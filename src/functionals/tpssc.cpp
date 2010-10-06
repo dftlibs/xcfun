b4e0d650f015dc6b9c69972800a34766d3de8d97
Ulf Ekstrom <uekstrom@gmail.com>
2010-10-06 16:17:26 +0200

    Add test for tpssc

--- src/functionals/tpssc.cpp
+++ src/functionals/tpssc.cpp
@@ -21,11 +21,22 @@ void setup_tpssc(functional &f)
 	     "Phys. Rev. Lett. 91 (2003) 146401\n"
 	     "Implemented by Andrea Debnarova\n");
   SET_MGGA_ENERGY_FUNCTION(f,energy);
-  // Test case from ??
- // const double d[1] = 
- //  {0};
- // const double ref[1] =
-  //  {0};
- // f.add_test(XC_VARS_AB,2,d,ref,1e-11);
+  // Test case from A**
+  const double d[] = {0.153652558932587,
+		      0.153652558932587,
+		      8.390981882024583E-002,
+		      8.390981882024583E-002,
+		      8.390981882024583E-002,
+		      6.826262722466284E-002,
+		      6.826262722466284E-002};
+  const double ref[] = {0,//Fill in energy
+			   -7.299596527683171E-002,
+		       -7.299596527683171E-002,
+		       1.247652223035057E-002,
+		       5.978495466748646E-002,
+		       1.247652223035057E-002,
+		       -4.068185412322921E-002,
+		       -4.068185412322921E-002};
+ // f.add_test(XC_VARS_AB,1,d,ref,1e-11);
 }
 
