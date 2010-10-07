6a59fc0aa92675224c62f755be108c1e19587223
Ulf Ekstrom <uekstrom@gmail.com>
2010-10-07 14:27:11 +0200

    Working (with exchange) GGA potential routine. Now for beta part and test with PBEc (has a-b cross terms).

--- src/xcfun.cpp
+++ src/xcfun.cpp
@@ -169,6 +169,57 @@ void xc_eval(xc_functional fun, int order,
   xc_eval_vec(fun,order,1,density,0,result,0);
 }
 
+// Compute the xc potential(s). This is simple for lda and more complicated for 
+// GGA's. For metaGAA's that depend on tau this is a non-local problem and 
+// cannot be solved by xcfun. Laplacian dependent metaGGA's could be implemented..
+void xc_potential(xc_functional fun, const double *density, double *e_xc, double *v_xc)
+{
+  if (fun->mode == XC_VARS_AB)
+    {
+      if (fun->type == XC_LDA) 
+	{
+	  double out[3];
+	  xc_eval(fun,1,density,out);
+	  e_xc[0] = out[0];
+	  v_xc[0] = out[1];
+	  v_xc[1] = out[2];
+	}
+      // Expecting lap_a and lap_b as element 5 and 6 of density
+      // Using that v_xc = dE/dn - div dE/dgradn
+      else if (fun->type == XC_GGA)  
+	{
+	  const int gaa = 2, gab = 3, gbb = 4, lapa = 5, lapb = 6;
+	  double out[25];
+	  xc_eval(fun,2,density,out);
+	  e_xc[0] = out[XC_D00000];
+
+	  v_xc[0] = out[XC_D10000];
+	  v_xc[0] -= 2*density[lapa]*out[XC_D00100] + density[lapb]*out[XC_D00010];
+	  v_xc[0] -= 2*(out[XC_D10100]*density[gaa] + 
+			out[XC_D01100]*density[gab] +
+			out[XC_D00200]*(2*density[lapa]*density[gaa]) +
+			out[XC_D00110]*(density[lapa]*density[gab] + density[lapb]*density[gaa]) +
+			out[XC_D00101]*(2*density[lapb]*density[gab]));
+	  v_xc[0] -= 2*(out[XC_D10010]*density[gab] +
+			out[XC_D01010]*density[gbb] +
+		        out[XC_D00110]*(2*density[lapa]*density[gab]) +
+			out[XC_D00020]*(density[lapb]*density[gab] + density[lapa]*density[gbb]) +
+			out[XC_D00011]*(2*density[lapb]*density[gbb]));
+
+	  v_xc[1] = 0;
+	}
+      else
+	{
+	  xcint_die("xc_potential() not implemented for metaGGA's",fun->type);
+	}
+    }
+  else
+    {
+      xcint_die("xc_potential() only implemented for AB mode",fun->mode);
+    }
+}
+
+
 void xc_contract(xc_functional fun, int order,
 		 const double *density, 
 		 double *result)
