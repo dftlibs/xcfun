a149501b80409bb98e42cc4fa4879718818e2640
Ulf Ekstrom <uekstrom@gmail.com>
2010-10-10 23:12:15 +0200

    TPSSx now works!

--- src/functionals/tpssx.cpp
+++ src/functionals/tpssx.cpp
@@ -2,12 +2,17 @@
 #include "constants.h"
 #include "tpssx_eps.h"
 
+using namespace tpssx_eps;
+
 template<class num>
-static num energy(const densvars<num> &d)
+static num tpssx_energy(const densvars<num> &d)
 {
-  num eps = tpssx_eps::tpssx_energy(d);
-  return eps;
-}   
+  num Fxa = F_x(2*d.a,4*d.gaa,2*d.taua);
+  num epsxunif_a = fx_unif(2*d.a);
+  num Fxb = F_x(2*d.b,4*d.gbb,2*d.taub);
+  num epsxunif_b = fx_unif(2*d.b);
+  return (epsxunif_a*Fxa + epsxunif_b*Fxb)/2;
+}
 
 void setup_tpssx(functional &f)
 {
@@ -20,12 +25,24 @@ void setup_tpssx(functional &f)
              "Designed for Molecules and Solids,\n"
 	     "Phys. Rev. Lett. 91 (2003) 146401\n"
 	     "Implemented by Andrea Debnarova\n");
-  SET_MGGA_ENERGY_FUNCTION(f,energy);
-  // Test case from ??
- // const double d[1] = 
- //  {0};
- // const double ref[1] =
-  //  {0};
- // f.add_test(XC_VARS_AB,2,d,ref,1e-11);
+  SET_MGGA_ENERGY_FUNCTION(f,tpssx_energy);
+  const double d[] = 
+    { 0.153652558932587,
+      0.153652558932587,
+      8.390981882024583E-002,
+      8.390981882024583E-002,
+      8.390981882024583E-002,
+      6.826262722466284E-002,
+      6.826262722466284E-002 };
+  const double ref[] =  // From ADF
+    {-1.7369862616505458e-01, //XCFun energy
+     -0.696647334686932,
+     -0.696647334686932,
+     -0.104380091839662,
+     0.0,
+     -0.104380091839662,     
+     0.128315600541666,       
+     0.128315600541666};
+  f.add_test(XC_VARS_AB,1,d,ref,1e-8);
 }
 
--- src/functionals/tpssx_eps.h
+++ src/functionals/tpssx_eps.h
@@ -9,53 +9,44 @@ namespace tpssx_eps
 {
 
   template<class num>
-  static num epsx_unif(const num &d)
+  static num fx_unif(const num &d)
   {
-    return -3*cbrt(3*PI2*d)/(4*PI);
+    return (-0.75*pow(3/PI,1.0/3.0))*pow(d,4.0/3.0);
   }
 
   template<class num>
   static num x(const num &d_n,const num &d_gnn, const num &d_tau) 
   {    
-    parameter kapa = 0.804;
-    parameter mu = 0.21951;
-    parameter b = 0.40;
-    parameter e = 1.573;
-    parameter c = 1.59096;
+    const parameter kappa = 0.804;
+    const parameter mu = 0.21951;
+    const parameter b = 0.40;
+    const parameter e = 1.537;
+    const parameter c = 1.59096;
     num p = d_gnn/(4*pow(3*PI2,2.0/3.0)*pow(d_n,8.0/3.0));
     num tauw = d_gnn/(8.0*d_n);
     num z = tauw/d_tau;
     num z2 = pow2(z);
     num tau_unif = 0.3*pow(3*PI2,2.0/3.0)*pow(d_n,5.0/3.0);
     num alpha = (d_tau - tauw)/tau_unif;         //  5*p*z/(3.0 - 3.0*z);
-    num q_b = 9*(alpha-1)/(20*sqrt(1+ b*alpha*(alpha-1))) + 2*p/3.0;
+    num q_b = (9.0/20.0)*(alpha-1)/sqrt(1+ b*alpha*(alpha-1)) + 2*p/3.0;
     num x_a = p*(10.0/81.0 + c*z2/pow2(1+z2));
     x_a += 146.0*pow2(q_b)/2025.0;
-    x_a -= 73.0*q_b*sqrt(0.5*0.6*0.6*z2 + 0.5*p*p)/405.0;
-    x_a += pow2(10.0*p/81.0)/kapa;
-    x_a += sqrt(e)*0.6*0.6*z2*20.0/81.0 + e*mu*pow3(p);
+    x_a -= 73.0/405.0*q_b*sqrt(0.5*0.6*0.6*z2 + 0.5*p*p);
+    x_a += pow2(p*10.0/81.0)/kappa;
+    x_a += 2*sqrt(e)*0.6*0.6*z2*10.0/81.0 + e*mu*pow3(p);
     return x_a/pow2(1+sqrt(e)*p);
   }
 
   template<class num>
   static num F_x(const num &d_n,const num &d_gnn, const num &d_tau)
   {
-    parameter kapa = 0.804;
+    const parameter kappa = 0.804;
     num xpz = x(d_n,d_gnn,d_tau);
-    return 1 + kapa - kapa/(1+xpz/kapa);
+    printf("x: ");
+    print_taylor(stdout,xpz);
+    printf("\n");
+    return 1 + kappa - kappa/(1+xpz/kappa);
   }
-
-  template<class num>
-  static num tpssx_energy(const densvars<num> &d)
-  {
-    num Fxa = F_x(2*d.a,4*d.gaa,2*d.taua);
-    num epsxunif_a = epsx_unif(2*d.a);
-    num Fxb = F_x(2*d.b,4*d.gbb,2*d.taub);
-    num epsxunif_b = epsx_unif(2*d.b);
-    return (epsxunif_a*Fxa*d.a + epsxunif_b*Fxb*d.b);
-  }
-
-
 }
 
 #endif
