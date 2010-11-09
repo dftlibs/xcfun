ece0fc58a2a1d409aa245725f382a1fc7457a3a9
Jonas Juselius <jonas.juselius@uit.no>
2010-11-09 16:36:08 +0100

    Bad idea... revert.

--- cmake/ConfigCompilerFlags.cmake
+++ cmake/ConfigCompilerFlags.cmake
@@ -27,10 +27,6 @@ if (NOT DEFINED DEFAULT_${_lang}_COMPILER_FLAGS_SET)
         CACHE STRING 
 		"Flags used by the compiler during release builds." FORCE)
 
-    set(CMAKE_${_lang}_LINK_FLAGS "${CMAKE_${_lang}_LINK_FLAGS}"
-        CACHE STRING 
-		"Flags used by the linker during linking." FORCE)
-
 	set (DEFAULT_${_lang}_COMPILER_FLAGS_SET ON 
 		CACHE INTERNAL 
 		"Flag that the default ${_lang} compiler flags have been set.")
