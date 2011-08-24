517af93f0718fb700529de6f405856f21612013f
Radovan Bast <radovan.bast@uit.no>
2011-08-24 15:08:27 +0200

    include kernel_example in "make test"

--- cmake/Tests.cmake
+++ cmake/Tests.cmake
@@ -1,1 +1,2 @@
-add_test(testall ${CMAKE_CURRENT_BINARY_DIR}/testall)
+add_test(testall        ${CMAKE_CURRENT_BINARY_DIR}/testall)
+add_test(kernel_example ${CMAKE_CURRENT_BINARY_DIR}/kernel_example)
