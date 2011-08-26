e76e813d44b927970182f90a6f4625efae4fce58
Radovan Bast <radovan.bast@uit.no>
2011-08-26 15:45:29 +0200

    changes from dirac repo

--- fortran/kernel_example.F90
+++ fortran/kernel_example.F90
@@ -11,15 +11,17 @@ program xc_example
 
    implicit none
 
-   integer, parameter   :: MAX_DENSITY_LENGTH = 100
    integer, parameter   :: NR_POINTS = 1
+   integer, parameter   :: NR_VARIABLES = 4
+
    character(1000)      :: text
    integer              :: id, order, ierr, ideriv, ipoint
-   real(8)              :: result_derv(4), result_derv_reference(4)
+   integer              :: vector_length
+   real(8)              :: res, res_reference(NR_VARIABLES)
+
    real(8), allocatable :: density(:, :, :)
    real(8), allocatable :: input_array(:, :)
    real(8), allocatable :: output_array(:, :)
-   real(8)              :: xc_energy_density
 
 !  print some info and copyright about the library
 !  please always include this info in your code
@@ -35,16 +37,6 @@ program xc_example
    call xc_set(id, XC_PBEX, 1.0d0)
    call xc_set(id, XC_PBEC, 1.0d0)
 
-!  fill in some phantasy values
-   allocate(density(0:MAX_DENSITY_LENGTH, 4, NR_POINTS))
-   density = 0.0d0
-   do ipoint = 1, NR_POINTS
-      density(0, 1, ipoint) = 1.0d0 !        n
-      density(0, 2, ipoint) = 2.0d0 !nabla_x n
-      density(0, 3, ipoint) = 3.0d0 !nabla_y n
-      density(0, 4, ipoint) = 4.0d0 !nabla_z n
-   end do
-
 !  First we just compute the energy, i.e. the 0-th order integrand.
 !  We have one gridpoint, and four variables, density N and gradient
 !  components NX NY NZ.
@@ -55,28 +47,39 @@ program xc_example
       stop 1
    endif
 
-   allocate(input_array(4, NR_POINTS))
-   allocate(output_array(1, NR_POINTS))
+   vector_length = 2**order
+!  allocate density and fill in some phantasy values
+   allocate(density(vector_length, NR_VARIABLES, NR_POINTS))
+   density = 0.0d0
+   do ipoint = 1, NR_POINTS
+      density(1, 1, ipoint) = 1.0d0 !        n
+      density(1, 2, ipoint) = 2.0d0 !nabla_x n
+      density(1, 3, ipoint) = 3.0d0 !nabla_y n
+      density(1, 4, ipoint) = 4.0d0 !nabla_z n
+   end do
+
+   allocate(input_array(NR_VARIABLES*vector_length, NR_POINTS))
+   allocate(output_array(vector_length, NR_POINTS))
 
    do ipoint = 1, NR_POINTS
-      input_array(:, ipoint) = (/density(0, 1, ipoint), &
-                                 density(0, 2, ipoint), &
-                                 density(0, 3, ipoint), &
-                                 density(0, 4, ipoint)/)
+      input_array(:, ipoint) = (/density(:, 1, ipoint), &
+                                 density(:, 2, ipoint), &
+                                 density(:, 3, ipoint), &
+                                 density(:, 4, ipoint)/)
    end do
-!  now compute the xc energy density at this point
    call xc_eval(id, NR_POINTS, input_array, output_array)
-   xc_energy_density = output_array(1, 1)
-   print *, 'The XC energy density is', xc_energy_density
+   res = output_array(vector_length, 1)
+   print *, 'The XC energy density is', res
 
+   deallocate(density)
    deallocate(input_array)
    deallocate(output_array)
 
 !  reference values for self test
-   result_derv_reference(1) = -0.97182658347124051d0
-   result_derv_reference(2) = -8.98025300594966838d-3
-   result_derv_reference(3) = -1.34703795089245043d-2
-   result_derv_reference(4) = -1.79605060118993368d-2
+   res_reference(1) = -0.97182658347124051d0
+   res_reference(2) = -8.98025300594966838d-3
+   res_reference(3) = -1.34703795089245043d-2
+   res_reference(4) = -1.79605060118993368d-2
 
 !  now let's compute the first derivatives ('potential')
 !  first set up xcfun for first derivatives
@@ -87,43 +90,57 @@ program xc_example
       stop 1
    endif
 
-   allocate(input_array(8, NR_POINTS))
-   allocate(output_array(2, NR_POINTS))
+   vector_length = 2**order
+!  allocate density and fill in some phantasy values
+   allocate(density(vector_length, NR_VARIABLES, NR_POINTS))
+   density = 0.0d0
+   do ipoint = 1, NR_POINTS
+      density(1, 1, ipoint) = 1.0d0 !        n
+      density(1, 2, ipoint) = 2.0d0 !nabla_x n
+      density(1, 3, ipoint) = 3.0d0 !nabla_y n
+      density(1, 4, ipoint) = 4.0d0 !nabla_z n
+   end do
+
+   allocate(input_array(NR_VARIABLES*vector_length, NR_POINTS))
+   allocate(output_array(vector_length, NR_POINTS))
 
 !  here is a tricky part, we need a loop and we need to put
 !  in a '1' where we want the derivative
 !  we loop over all variables we want a derivative with respect to
-   do ideriv = 1, 4
+   do ideriv = 1, NR_VARIABLES
       do ipoint = 1, NR_POINTS
-         density(1, 1:4, ipoint)    = 0.0d0
-         density(1, ideriv, ipoint) = 1.0d0
-         input_array(:, ipoint) = (/density(0:1, 1, ipoint), &
-                                    density(0:1, 2, ipoint), &
-                                    density(0:1, 3, ipoint), &
-                                    density(0:1, 4, ipoint)/)
+         density(2, :,      ipoint) = 0.0d0
+         density(2, ideriv, ipoint) = 1.0d0
       end do
-!     It is possible to put in other numbers than 1 and 0 in the density2 array
+!     It is possible to put in other numbers than 1 and 0
 !     this is where perturbed densities go for automatic contraction
 
+      do ipoint = 1, NR_POINTS
+         input_array(:, ipoint) = (/density(:, 1, ipoint), &
+                                    density(:, 2, ipoint), &
+                                    density(:, 3, ipoint), &
+                                    density(:, 4, ipoint)/)
+      end do
       call xc_eval(id, NR_POINTS, input_array, output_array)
-      result_derv(ideriv) = output_array(2, 1) !this is first point
-      print *, 'dE/dx_i for i =', ideriv, 'is', result_derv(ideriv)
+      res = output_array(vector_length, 1)
+      print *, 'dE/dx_i for i =', ideriv, 'is', res
 
 !     test against reference numbers
-      if (abs(result_derv(ideriv) - result_derv_reference(ideriv)) > 1.0d-6) then
+      if (abs(res - res_reference(ideriv)) > 1.0d-6) then
          print *, 'error: derivatives do not match reference numbers'
          stop 1
       end if
    end do
 
+   deallocate(density)
    deallocate(input_array)
    deallocate(output_array)
 
 !  reference values for self test
-   result_derv_reference(1) = -2.0795504461938754d0
-   result_derv_reference(2) =  2.14893341430147031d-2
-   result_derv_reference(3) =  4.12142542204717230d-2
-   result_derv_reference(4) =  6.09391742979287290d-2
+   res_reference(1) = -2.0795504461938754d0
+   res_reference(2) =  2.14893341430147031d-2
+   res_reference(3) =  4.12142542204717230d-2
+   res_reference(4) =  6.09391742979287290d-2
 
 !  now second derivative of the "potential", contracted with one perturbed density.
 !  hopefully the strange input/output_array format will start to make sense
@@ -134,50 +151,56 @@ program xc_example
       stop 1
    endif
 
+   vector_length = 2**order
+!  allocate density and fill in some phantasy values
+   allocate(density(vector_length, NR_VARIABLES, NR_POINTS))
    density = 0.0d0
    do ipoint = 1, NR_POINTS
-      density(0, 1, ipoint) = 1.0d0
-      density(0, 2, ipoint) = 2.0d0
-      density(0, 3, ipoint) = 3.0d0
-      density(0, 4, ipoint) = 4.0d0
-      density(1, 1, ipoint) = 5.0d0
-      density(1, 2, ipoint) = 6.0d0
-      density(1, 3, ipoint) = 7.0d0
-      density(1, 4, ipoint) = 8.0d0
+      density(1, 1, ipoint) = 1.0d0
+      density(1, 2, ipoint) = 2.0d0
+      density(1, 3, ipoint) = 3.0d0
+      density(1, 4, ipoint) = 4.0d0
+      density(2, 1, ipoint) = 5.0d0
+      density(2, 2, ipoint) = 6.0d0
+      density(2, 3, ipoint) = 7.0d0
+      density(2, 4, ipoint) = 8.0d0
    end do
 
-   allocate(input_array(16, NR_POINTS))
-   allocate(output_array(4, NR_POINTS))
+   allocate(input_array(NR_VARIABLES*vector_length, NR_POINTS))
+   allocate(output_array(vector_length, NR_POINTS))
 
 !  now we have to put in ones and zeroes again to generate derivatives (which then generate matrix elements in ADF)
-!  density(0, :, :)  unperturbed values
-!  density(1, :, :)  perturbed values
-!  density(2, :, :)  ones and zeros
-!  density(3, :, :)  not used
+!  density(1, :, :)  unperturbed values
+!  density(2, :, :)  perturbed values
+!  density(3, :, :)  ones and zeros
+!  density(4, :, :)  not used
 !  in general there will be 2^order elements, 1 ground state, 2^order-2 perturbed and one unused.
-   do ideriv = 1, 4
+   do ideriv = 1, NR_VARIABLES
       do ipoint = 1, NR_POINTS
-         density(2, 1:4, ipoint)    = 0.0d0
-         density(2, ideriv, ipoint) = 1.0d0
-         input_array(:, ipoint) = (/density(0:3, 1, ipoint), &
-                                    density(0:3, 2, ipoint), &
-                                    density(0:3, 3, ipoint), &
-                                    density(0:3, 4, ipoint)/)
+         density(3, :,      ipoint) = 0.0d0
+         density(3, ideriv, ipoint) = 1.0d0
       end do
 !     It is possible to put in other numbers than 1 and 0 in the density2 array
 !     this is where perturbed densities go for automatic contraction
 
+      do ipoint = 1, NR_POINTS
+         input_array(:, ipoint) = (/density(:, 1, ipoint), &
+                                    density(:, 2, ipoint), &
+                                    density(:, 3, ipoint), &
+                                    density(:, 4, ipoint)/)
+      end do
       call xc_eval(id, NR_POINTS, input_array, output_array)
-      result_derv(ideriv) = output_array(4, 1) !this is first point
-      print *, 'd^2 E/dx_i dD_pert for i =', ideriv, 'is', result_derv(ideriv)
+      res = output_array(vector_length, 1)
+      print *, 'd^2 E/dx_i dD_pert for i =', ideriv, 'is', res
 
 !     test against reference numbers
-      if (abs(result_derv(ideriv) - result_derv_reference(ideriv)) > 1.0d-6) then
+      if (abs(res - res_reference(ideriv)) > 1.0d-6) then
          print *, 'error: derivatives do not match reference numbers'
          stop 1
       end if
    end do
 
+   deallocate(density)
    deallocate(input_array)
    deallocate(output_array)
 
@@ -199,6 +222,6 @@ program xc_example
 
 !  Note3: You can extend this example trivially to alpha/beta densities
 !  By specifying XC_A_B_AX_AY_AZ_BX_BY_BZ instead of  XC_N_NX_NY_NZ
-!  Then the number 4 above will be replaced by 8.
+!  Then the NR_VARIABLES becomes 8.
 
 end program
