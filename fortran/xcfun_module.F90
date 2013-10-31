7a5e138931a4b35a00d3615a5fd5604b54a7e928
Ulf Ekstrom <uekstrom@gmail.com>
2013-10-31 14:00:01 +0100

    Update the variable sets in the fortran module.

--- fortran/xcfun_module.F90
+++ fortran/xcfun_module.F90
@@ -29,18 +29,20 @@ module xcfun
   XCFUN_INTEGER, parameter :: XC_A_B_GAA_GAB_GBB_TAUA_TAUB = 13
   XCFUN_INTEGER, parameter :: XC_N_S_GNN_GNS_GSS_LAPN_LAPS = 14
   XCFUN_INTEGER, parameter :: XC_N_S_GNN_GNS_GSS_TAUN_TAUS = 15
-  XCFUN_INTEGER, parameter :: XC_A_AX_AY_AZ = 16
-  XCFUN_INTEGER, parameter :: XC_A_B_AX_AY_AZ_BX_BY_BZ = 17
-  XCFUN_INTEGER, parameter :: XC_N_NX_NY_NZ = 18
-  XCFUN_INTEGER, parameter :: XC_N_S_NX_NY_NZ_SX_SY_SZ = 19
-  XCFUN_INTEGER, parameter :: XC_A_AX_AY_AZ_TAUA = 20
-  XCFUN_INTEGER, parameter :: XC_A_B_AX_AY_AZ_BX_BY_BZ_TAUA_TAUB = 21
-  XCFUN_INTEGER, parameter :: XC_N_NX_NY_NZ_TAUN = 22
-  XCFUN_INTEGER, parameter :: XC_N_S_NX_NY_NZ_SX_SY_SZ_TAUN_TAUS = 23
-  XCFUN_INTEGER, parameter :: XC_A_2ND_TAYLOR = 24
-  XCFUN_INTEGER, parameter :: XC_A_B_2ND_TAYLOR = 25
-  XCFUN_INTEGER, parameter :: XC_N_2ND_TAYLOR = 26
-  XCFUN_INTEGER, parameter :: XC_N_S_2ND_TAYLOR = 27
+  XCFUN_INTEGER, parameter :: XC_A_B_GAA_GAB_GBB_LAPA_LAPB_TAUA_TAUB = 16
+  XCFUN_INTEGER, parameter :: XC_N_S_GNN_GNS_GSS_LAPN_LAPS_TAUN_TAUS = 17
+  XCFUN_INTEGER, parameter :: XC_A_AX_AY_AZ = 18
+  XCFUN_INTEGER, parameter :: XC_A_B_AX_AY_AZ_BX_BY_BZ = 19
+  XCFUN_INTEGER, parameter :: XC_N_NX_NY_NZ = 20
+  XCFUN_INTEGER, parameter :: XC_N_S_NX_NY_NZ_SX_SY_SZ = 21
+  XCFUN_INTEGER, parameter :: XC_A_AX_AY_AZ_TAUA = 22
+  XCFUN_INTEGER, parameter :: XC_A_B_AX_AY_AZ_BX_BY_BZ_TAUA_TAUB = 23
+  XCFUN_INTEGER, parameter :: XC_N_NX_NY_NZ_TAUN = 24
+  XCFUN_INTEGER, parameter :: XC_N_S_NX_NY_NZ_SX_SY_SZ_TAUN_TAUS = 25
+  XCFUN_INTEGER, parameter :: XC_A_2ND_TAYLOR = 26
+  XCFUN_INTEGER, parameter :: XC_A_B_2ND_TAYLOR = 27
+  XCFUN_INTEGER, parameter :: XC_N_2ND_TAYLOR = 28
+  XCFUN_INTEGER, parameter :: XC_N_S_2ND_TAYLOR = 29
 
 ! Indices into the output array of derivatives. Fortran numbering.
   XCFUN_INTEGER, parameter :: XC_D0 = 1
