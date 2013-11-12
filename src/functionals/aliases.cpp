a10ec63b4fa8d272b1d43569d73ae1a0181db67a
Ulf Ekstrom <uekstrom@gmail.com>
2013-11-12 13:40:36 +0100

    Fix alias for BP86c

--- src/functionals/aliases.cpp
+++ src/functionals/aliases.cpp
@@ -5,7 +5,7 @@ static struct alias_data aliases_array[XC_MAX_ALIASES] =
   {"lda","Slater exchange and VWN5 correlation",{{"slaterx",1.0},{"vwn5c",1.0}} },
   {"blyp","Becke exchange and LYP correlation",{{"beckex",1.0},{"lypc",1.0}} },
   {"pbe","PBE exchange and correlation",{{"pbex",1.0},{"pbec",1.0}} },
-  {"bp86","Becke-Perdew 1986",{{"beckex",1.0},{"p86c",1.0},{"pz81c",1.0}} },
+  {"bp86","Becke-Perdew 1986",{{"beckex",1.0},{"p86c",1.0}} },
   {"kt2","Keal-Tozer 2",{{"slaterx",1.07173},{"ktx",-0.006},{"vwn5c",0.576727}} },
   {"kt3","Keal-Tozer 3",{{"slaterx",1.092},{"ktx",-0.004},{"optxcorr",-0.925452},{"lypc",0.864409}} },
   // Hybrid Functionals
@@ -26,7 +26,7 @@ static struct alias_data aliases_array[XC_MAX_ALIASES] =
   {"svwn3","Slater exchange and VWN3 correlation",{{"slaterx",1.0},{"vwn3c",1.0}} },
   {"b3p86","Becke-3-paramater-LYP (VWN5 form)", {{"slaterx",0.80},{"beckecorrx",0.72},{"p86c",0.81},{"vwn5c",1.0},{"exx",0.20}} }, 
   {"b3p86-g","Becke-3-paramater-LYP (VWN3 form)", {{"slaterx",0.80},{"beckecorrx",0.72},{"p86c",0.81},{"vwn3c",1.0},{"exx",0.20}} }, 
-  {"bpw91","Becke 88 exchange+PW91",{{"beckex",1.0},{"pw91cc",1.0}} },
+  {"bpw91","Becke 88 exchange+PW91",{{"beckex",1.0},{"pw91c",1.0}} },
 };
 
 // TODO: OLYP
