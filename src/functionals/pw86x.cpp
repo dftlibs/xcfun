f58f02a5c77525a287b428d2929acf37c0e6da2f
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-04-04 09:38:45 -0400

    Update license headers

--- src/functionals/pw86x.cpp
+++ src/functionals/pw86x.cpp
@@ -1,3 +1,17 @@
+/*
+ * XCFun, an arbitrary order exchange-correlation library
+ * Copyright (C) 2018 Ulf Ekström and contributors.
+ *
+ * This file is part of XCFun.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * For information on the complete list of contributors to the
+ * XCFun library, see: <https://xcfun.readthedocs.io/>
+ */
+
 #include "functional.hpp"
 
 template<class num> 
