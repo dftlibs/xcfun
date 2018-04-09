af12af6d8ec8b0a0db55dcfe1186b43ecee69085
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-04-08 23:45:41 -0400

    Localize Fortran stuff to fortran folder

--- cmake/custom/int64.cmake
+++ cmake/custom/int64.cmake
@@ -2,25 +2,13 @@
 # licensed under BSD-3: https://github.com/coderefinery/autocmake/blob/master/LICENSE
 # Simplified and adapted by Roberto Di Remigio
 
-#.rst:
-#
-# Enables 64-bit integer support for Fortran projects.
-#
-# Variables modified (provided the corresponding language is enabled)::
-#
-#   CMAKE_Fortran_FLAGS
-#
-# autocmake.yml configuration::
-#
-#   docopt: "--int64 Enable 64bit integers [default: False]."
-#   define: "'-DENABLE_64BIT_INTEGERS={0}'.format(arguments['--int64'])"
-
 option_with_print(
   NAME
     ENABLE_64BIT_INTEGERS
   MESSAGE
     "Enable 64-bit integers"
-  DEFAULT OFF
+  DEFAULT
+    OFF
   )
 
 if(ENABLE_64BIT_INTEGERS)
--- cmake/custom/xcfun.cmake
+++ cmake/custom/xcfun.cmake
@@ -1,4 +1,3 @@
-# List options
 option_with_default(
   NAME
     XCFun_XC_MAX_ORDER
@@ -12,5 +11,3 @@ if(DEFINED XCFun_XC_MAX_ORDER AND XCFun_XC_MAX_ORDER LESS 3)
   message(STATUS "${XCFun_XC_MAX_ORDER} not a valid value for maximum order of XC kernel derivatives! Resetting to its default value 3")
   set(XCFun_XC_MAX_ORDER 3 CACHE STRING "Maximum order of derivatives of the exchange-correlation kernel" FORCE)
 endif()
-# FIXME Localize use of this option
-add_definitions("-DXC_MAX_ORDER=${XCFun_XC_MAX_ORDER}")
