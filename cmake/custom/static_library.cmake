b9c71ba6921a539e9480c5f0b4c36503ecde0b07
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-04-09 23:11:52 -0400

    Can compile static and shared library

---
+++ cmake/custom/static_library.cmake
@@ -0,0 +1,101 @@
+#.rst:
+#
+# Enables creation of static/shared library.
+# If the shared library is created, make it as static as possible.
+#
+# Variables modified (provided the corresponding language is enabled)::
+#
+#   XCFun_Fortran_FLAGS
+#   XCFun_C_FLAGS
+#   XCFun_CXX_FLAGS
+#
+# autocmake.yml configuration::
+#
+#   docopt: "--static Create only the static library [default: False]."
+#   define: "'-DSTATIC_LIBRARY_ONLY={0}'.format(arguments['--static'])"
+
+
+option_with_print(
+  NAME
+    STATIC_LIBRARY_ONLY
+  MESSAGE
+    "Create the static library only"
+  DEFAULT
+    OFF
+  )
+option_with_print(
+  NAME
+    SHARED_LIBRARY_ONLY
+  MESSAGE
+    "Create the shared library only"
+  DEFAULT
+    OFF
+  )
+option_with_print(
+  NAME
+    ENABLE_GENERIC
+  MESSAGE
+    "Enable mostly static linking in shared library"
+  DEFAULT
+    OFF
+  )
+
+if(ENABLE_GENERIC)
+  if(DEFINED CMAKE_Fortran_COMPILER_ID)
+    if(CMAKE_Fortran_COMPILER_ID MATCHES GNU)
+      list(APPEND XCFun_Fortran_FLAGS
+        "-static-libgfortran"
+        )
+    endif()
+    if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
+      list(APPEND XCFun_Fortran_FLAGS
+        "-static-libgcc"
+        "-static-intel"
+        )
+    endif()
+  endif()
+
+  if(DEFINED CMAKE_C_COMPILER_ID)
+    if(CMAKE_C_COMPILER_ID MATCHES GNU)
+      list(APPEND XCFun_C_FLAGS
+        "-static-libgcc"
+        "-fpic"
+        )
+    endif()
+    if(CMAKE_C_COMPILER_ID MATCHES Intel)
+      list(APPEND XCFun_C_FLAGS
+        "-static-libgcc"
+        "-static-intel"
+        "-wd10237"
+        )
+    endif()
+    if(CMAKE_C_COMPILER_ID MATCHES Clang)
+      list(APPEND XCFun_C_FLAGS
+        "-fpic"
+        )
+    endif()
+  endif()
+
+  if(DEFINED CMAKE_CXX_COMPILER_ID)
+    if(CMAKE_CXX_COMPILER_ID MATCHES GNU)
+      list(APPEND XCFun_CXX_FLAGS
+        "-static-libstdc++"
+        "-static-libgcc"
+        )
+    endif()
+    if(CMAKE_CXX_COMPILER_ID MATCHES Intel)
+      list(APPEND XCFun_CXX_FLAGS
+        "-Wl,--as-needed"
+        "-static-libstdc++"
+        "-static-libgcc"
+        "-static-intel"
+        "-wd10237"
+        )
+    endif()
+    if(CMAKE_CXX_COMPILER_ID MATCHES Clang)
+      list(APPEND XCFun_CXX_FLAGS
+        "-static-libstdc++"
+        )
+    endif()
+  endif()
+endif()
