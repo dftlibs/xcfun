7ab83fda500e090a1b22bd95c1c322a537222246
Radovan Bast <bast@users.noreply.github.com>
2018-04-17 15:02:05 +0200

    fortran support is off by default

--- cmake/custom/fc_optional.cmake
+++ cmake/custom/fc_optional.cmake
@@ -34,7 +34,7 @@
 #   docopt:
 #     - "--fc=<FC> Fortran compiler [default: gfortran]."
 #     - "--extra-fc-flags=<EXTRA_FCFLAGS> Extra Fortran compiler flags [default: '']."
-#     - "--fc-support=<FC_SUPPORT> Toggle Fortran language support (ON/OFF) [default: ON]."
+#     - "--fc-support=<FC_SUPPORT> Toggle Fortran language support (ON/OFF) [default: OFF]."
 #   define:
 #     - "'-DCMAKE_Fortran_COMPILER={0} -DEXTRA_FCFLAGS=\"{1}\"'.format(arguments['--fc'], arguments['--extra-fc-flags'])"
 #     - "'-DENABLE_FC_SUPPORT={0}'.format(arguments['--fc-support'])"
