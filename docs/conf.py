c85853f19ddead1a831df2ed1bd6567530b94d62
Stig Rune Jensen <stig.r.jensen@uit.no>
2020-02-03 10:48:26 +0100

    Miscellanous fixes for documentation (#115)
    * Add sphinx_rtd_theme to requirements.txt
    
    * Add xcfun logo to RTD sidebar
    
    * Fix examples/README
    
    Co-authored-by: Roberto Di Remigio <robertodr@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,10 @@ if on_rtd:
 else:
     html_theme = 'sphinx_rtd_theme'
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = '_static/logo2.png'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
