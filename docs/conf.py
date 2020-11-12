4787b8703acd5f5e31fcd47d53b1fd9f58190f5b
Roberto Di Remigio <robertodr@users.noreply.github.com>
2020-11-12 08:48:36 +0100

    Bump version in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, Ulf Ekström and contributors'
 author = 'Ulf Ekström and contributors'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.0'
+release = '2.1.1'
 
 # -- General configuration ---------------------------------------------------
 
